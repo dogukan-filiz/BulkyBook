7ef94ef1218ec31ad703f18ca7ce22ce47de1889
DoukanFiliz <dogukanfiliz@hotmail.com>
2023-07-19 02:15:22 +0300

    New functionality and visualization
    Functionality and visualization are added for new operation which is named "Update"

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,11 +36,47 @@ namespace BulkyBookWeb.Controllers
                 }
             }
 
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-                return Redirect("Index");
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id); --Other approaches
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u=>u.Id == id);
+
+            if (categoryFromDb == null)
+                return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category obj)
+        {
+            if (obj.DisplayOrder != null)
+            {
+                if (obj.Name == obj.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(obj);
         }
