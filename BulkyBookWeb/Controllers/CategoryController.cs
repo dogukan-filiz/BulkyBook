e6017e1ba448ebe373829b84923c23ee90f49cad
DoukanFiliz <dogukanfiliz@hotmail.com>
2023-07-18 23:41:24 +0300

    Visualising
    Created view page for "Create" button

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,9 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
